0d9128e719149215eded7848676bf2344b4383ce
elicopteriu <34962716+elicopteriu@users.noreply.github.com>
2017-12-30 16:44:54 +0200

    Initial commit

---
+++ wallpapergrab.py
@@ -0,0 +1,75 @@
+import ctypes
+import praw
+import os
+import requests
+import shutil
+import io
+
+from pathlib import Path
+
+print("Setting directory...")
+directory = os.getcwd()
+directory += "\\" + "image.bmp"
+directory_jpg = os.getcwd()
+directory_jpg = directory_jpg + "\\" + "image.jpg"
+
+print("Checking if older image exists...")
+image=Path(directory)
+if image.is_file():
+        os.remove(directory)
+        print("Deleting .bmp file")
+
+
+
+image_jpg=Path(directory_jpg)
+if image_jpg.is_file():
+        os.remove(directory_jpg)
+        print("Deleting .jpg file")
+
+
+
+
+
+r = praw.Reddit(user_agent='WallpaperGrab')
+ 
+                     
+print("Fetching wallpaper..")
+wallpaper=r.get_subreddit('wallpapers').get_hot(limit=1)
+for item in wallpaper:
+        stock_URL = item.url
+
+
+end = ".jpg"
+if stock_URL.endswith(end):
+    direct_URL = stock_URL
+    
+    
+else:
+    direct_URL = stock_URL + '.jpg'
+
+print("Downloading wallpaper..")
+print(direct_URL)
+download = requests.get(direct_URL)
+with io.open(directory_jpg, 'wb') as file:
+        file.write(download.content)
+
+
+os.rename('image.jpg','image.bmp')
+
+
+
+
+
+SPI_SETDESKWALLPAPER = 0x14     #which command (20)
+
+SPIF_UPDATEINIFILE   = 0x2 #forces instant update
+src = directory #full file location
+#in python 3.4 you have to add 'r' before "path\img.jpg"
+
+print(ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, src, SPIF_UPDATEINIFILE))
+#SystemParametersInfoW instead of SystemParametersInfoA (W instead of A)
+
+print("Wallpaper set")
+print("Done")
+
+
